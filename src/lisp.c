7fe8576c059a500e2673921c8fc78761b7326413
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-02-04 19:38:09 +0000

    Move sys.c into lisp.c

--- src/lisp.c
+++ src/lisp.c
@@ -1,11 +1,10 @@
-/* sys.c: System management
+/* lisp.c: Lisp constructors/destructors
  * Created: 2025-08-20
  * Author: Aryadev Chavali
  * License: See end of file
  * Commentary:
  */
 
-#include <assert.h>
 #include <malloc.h>
 #include <string.h>
 
@@ -62,6 +61,53 @@ void sys_cleanup(sys_t *sys)
 
   // Ensure no one treats this as active in any sense
   memset(sys, 0, sizeof(*sys));
+}
+
+lisp_t *make_int(i64 i)
+{
+  return tag_int(i);
+}
+
+lisp_t *cons(sys_t *sys, lisp_t *car, lisp_t *cdr)
+{
+  cons_t *cons = calloc(1, sizeof(*cons));
+  cons->car    = car;
+  cons->cdr    = cdr;
+
+  lisp_t *lcons = tag_cons(cons);
+  sys_register(sys, lcons);
+  return lcons;
+}
+
+lisp_t *make_vec(sys_t *sys, u64 capacity)
+{
+  vec_t *vec = calloc(1, sizeof(*vec));
+  vec_init(vec, capacity);
+  lisp_t *ptr = tag_vec(vec);
+  sys_register(sys, ptr);
+  return ptr;
+}
+
+lisp_t *intern(sys_t *sys, sv_t sv)
+{
+  char *str = sym_table_find(&sys->symtable, sv);
+  return tag_sym(str);
+}
+
+lisp_t *car(lisp_t *lsp)
+{
+  if (!IS_TAG(lsp, CONS))
+    return NIL;
+  else
+    return CAR(lsp);
+}
+
+lisp_t *cdr(lisp_t *lsp)
+{
+  if (!IS_TAG(lsp, CONS))
+    return NIL;
+  else
+    return CDR(lsp);
 }
 
 /* Copyright (C) 2025, 2026 Aryadev Chavali
