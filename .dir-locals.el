edb2f5c5c8708d821d1656a0a3ff9b05de18a2db
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-02-05 04:34:29 +0000

    dir-locals: run testing, then examples for default compile-command
    By default we should test all our code for regressions.  I'm always
    running the examples anyway to test new features, so we should have
    that recipe run afterwards.
    
    If a test fails, that's first priority to fix.
    
    If an example fails, time to continue working.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((compile-command . "make MODE=debug")
+((nil . ((compile-command . "make MODE=debug test examples")
          (+license/license-choice . "GNU General Public License Version 2")))
  (c-mode . ((mode . clang-format))))
