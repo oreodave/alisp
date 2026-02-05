010895331decd1d8d7f2b8d5d5c781d0fa32598e
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-02-05 03:59:43 +0000

    examples: Added an example for hello world

---
+++ examples/hello-world.lisp
@@ -0,0 +1,14 @@
+;;; hello-world.lisp - 2026-02-04
+
+;; Copyright (C) 2026 Aryadev Chavali
+
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE.  See the GNU General Public License Version 2 for
+;; details.
+
+;; You may distribute and modify this code under the terms of the GNU General
+;; Public License Version 2, which you should have received a copy of along with
+;; this program.  If not, please go to <https://www.gnu.org/licenses/>.
+
+(display "Hello, world!")
