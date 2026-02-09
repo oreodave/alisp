b8d0ee2c7f8de5f6a8474ee8295afabdc414dd42
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-02-09 09:57:46 +0000

    .dir-locals: added compile-command for testing
    When I'm writing unit test code, it's nicer to compile with full logs
    by default.

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,4 +3,6 @@
 
 ((nil . ((compile-command . "make MODE=debug test examples")
          (+license/license-choice . "GNU General Public License Version 2")))
- (c-mode . ((mode . clang-format))))
+ (c-mode . ((mode . clang-format)))
+ ("test" .
+  ((nil . ((compile-command . "make MODE=full test"))))))
