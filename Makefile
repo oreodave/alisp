929ed2b2762ee61cbc29d013522ce598856a79b8
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-02-13 00:06:32 +0000

    allocator: implement a basic allocator

--- Makefile
+++ Makefile
@@ -19,7 +19,8 @@ CFLAGS=$(GFLAGS) $(DFLAGS) -DTEST_VERBOSE=1
 endif
 
 # Units to compile
-UNITS=src/sv.c src/vec.c src/stream.c src/symtable.c src/lisp.c src/sys.c src/reader.c
+UNITS=src/sv.c src/vec.c src/stream.c src/symtable.c src/lisp.c src/allocator.c \
+		src/sys.c src/reader.c
 OBJECTS:=$(patsubst src/%.c, $(DIST)/%.o, $(UNITS))
 
 TEST_UNITS=test/main.c
--- include/alisp/allocator.h
+++ include/alisp/allocator.h
@@ -1,21 +1,45 @@
-/* alisp.h: All includes
- * Created: 2026-02-04
+/* allocator.h: Lisp Allocator
+ * Created: 2026-02-12
  * Author: Aryadev Chavali
  * License: See end of file
  * Commentary:
  */
 
-#ifndef ALISP_H
-#define ALISP_H
+#ifndef ALLOCATOR_H
+#define ALLOCATOR_H
 
-#include <alisp/allocator.h>
-#include <alisp/base.h>
 #include <alisp/lisp.h>
-#include <alisp/reader.h>
-#include <alisp/stream.h>
-#include <alisp/sv.h>
-#include <alisp/symtable.h>
 #include <alisp/vec.h>
+
+#define ALLOC_PAGE_DEFAULT_SIZE 512
+
+typedef struct
+{
+  u64 references;
+  tag_t tag : 8;
+} alloc_metadata_t;
+
+typedef struct
+{
+  alloc_metadata_t metadata;
+  u8 data[];
+} alloc_node_t;
+
+typedef struct
+{
+  vec_t data;
+} page_t;
+
+typedef struct
+{
+  vec_t pages;
+  vec_t free_list;
+} arena_t;
+
+lisp_t *arena_make(arena_t *, tag_t type);
+void arena_delete(arena_t *, lisp_t *);
+u64 arena_cost(arena_t *);
+void arena_free(arena_t *);
 
 #endif
 
